849c0db6a7e5d74125c5c49494f0ed5f29abd572
Lance <lance.ye@66ifuel.com>
2020-06-15 10:22:34 +0800

    add 1.0q

---
+++ db/sqlite.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	_ "github.com/mattn/go-sqlite3"
+	"gitlab.66ifuel.com/golang-tools/golib/config"
+	"strings"
+	"xorm.io/xorm"
+)
+
+var engine *xorm.Engine
+
+func InitSqliteDb() (err error) {
+	var dbPath string
+	dbPath = config.Cfg.Sqlite.DbPath
+	strings.TrimSuffix(dbPath, "/")
+	dbPath = dbPath + "/" + config.Cfg.Sqlite.DbName + ".db"
+	if engine, err = xorm.NewEngine("sqlite3", dbPath); err != nil {
+		return err
+	}
+	return nil
+}
+
+func QuerySqliteEngine() *xorm.Engine {
+	return engine
+}
