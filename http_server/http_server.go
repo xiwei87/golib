a59493974cd9c6e48dbc40742d9f9dec753b7069
Lance <lance.ye@66ifuel.com>
2020-07-02 10:30:40 +0800

    数据库公共库整理

--- http_server/http_server.go
+++ http_server/http_server.go
@@ -55,7 +55,7 @@ func NewHttpServer() *HttpServer {
 
 func (s *HttpServer) StartServer(cfg *HttpCfg) error {
 	if cfg == nil {
-		return errors.New("配置文件为空")
+		return errors.New("HTTP configuration information is empty")
 	}
 	addr := ":" + strconv.Itoa(cfg.ListenPort)
 	server = &http.Server{
@@ -78,34 +78,33 @@ func (s *HttpServer) StopServer() error {
 func (s *HttpServer) printAccessLog() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		/* init logid, start_time, request_id, method, remoteAddr */
-		s.Request.startTime = time.Now()
-		s.Request.httpMethod = c.Request.Method
-		s.Request.requestId = utils.NewRequestId()
-		s.Request.remoteAddr = c.ClientIP()
+		s.Request.StartTime = time.Now()
+		s.Request.HttpMethod = c.Request.Method
+		s.Request.RequestId = utils.NewRequestId()
+		s.Request.RequestTime = c.ClientIP()
 		if userAgent := c.Request.Header.Get("User-Agent"); userAgent != "" {
-			s.Request.userAgent = userAgent
+			s.Request.UserAgent = userAgent
 		} else {
-			s.Request.userAgent = "unknown"
+			s.Request.UserAgent = "unknown"
 		}
 		/* init response */
-		s.Response.status = http.StatusOK
-		s.Response.errCode = common.CODE_OK
+		s.Response.Status = http.StatusOK
+		s.Response.ErrCode = common.CODE_OK
 		c.Header("Server", "66ifuel/1.0.0")
-		c.Header("X-Request-Id", s.Request.requestId)
+		c.Header("X-Request-Id", s.Request.RequestId)
 		// Process request
 		c.Next()
 		// Print Access Log
-		requestTime := (time.Now().UnixNano() - s.Request.startTime.UnixNano()) / 1e6
+		requestTime := (time.Now().UnixNano() - s.Request.StartTime.UnixNano()) / 1e6
 		slog.Infof("errno[%d] ip[%s] logId[%s] uri[%s] cost[%d] status[%d] ua[%s] request done",
-			s.Response.errCode, s.Request.remoteAddr,
-			s.Request.requestId, c.Request.URL.Path, requestTime,
-			s.Response.status, s.Request.userAgent)
+			s.Response.ErrCode, s.Request.RemoteAddr, s.Request.RequestId, c.Request.URL.Path, requestTime,
+			s.Response.Status, s.Request.UserAgent)
 	}
 }
 
 func (s *HttpServer) NoMethodHandler(ctx *gin.Context) {
 	format := `{"request_id":"%s","err_code":%d,"err_msg":"%s"}`
-	resStr := fmt.Sprintf(format, s.Request.requestId, common.CODE_NO_METHOD, "method not allowed")
+	resStr := fmt.Sprintf(format, s.Request.RequestId, common.CODE_NO_METHOD, "method not allowed")
 	ctx.Header("Content-Type", "application/json; charset=utf-8")
 	ctx.Writer.Write([]byte(resStr))
 }
